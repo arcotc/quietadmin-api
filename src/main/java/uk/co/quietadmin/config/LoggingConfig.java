744d28241475b008d1df811082269258cf56463b
markhunter <mark@theworshipbible.church>
2026-02-13 11:35:22 +0000

    Improve logging configuration

---
+++ src/main/java/uk/co/quietadmin/config/LoggingConfig.java
@@ -0,0 +1,18 @@
+package uk.co.quietadmin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class LoggingConfig {
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeClientInfo(true);
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(false);
+        filter.setIncludeHeaders(false);
+        return filter;
+    }
+}
